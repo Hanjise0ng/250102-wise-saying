0e26ed3335226752979686c5279ed287b8813132
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-06 10:45:34 +0900

    command test5 green

--- src/main/java/app/global/Command.java
+++ src/main/java/app/global/Command.java
@@ -3,10 +3,23 @@ package app.global;
 public class Command {
 
     String actionName;
+    String paramKey;
+    String paramValue;
 
     public Command(String cmd) {
-        String[] cmdBits = cmd.split("\\?");
+        String[] cmdBits = cmd.split("\\?"); // [삭제, id=1]
         actionName = cmdBits[0];
+
+        if(cmdBits.length < 2) {
+            paramValue = "";
+            return;
+        }
+
+        String param = cmdBits[1];
+
+        String[] paramBits = param.split("=");
+        paramKey = paramBits[0];
+        paramValue = paramBits[1];
     }
 
     public String getActionName() {
@@ -14,6 +27,6 @@ public class Command {
     }
 
     public int getParam() {
-        return 0;
+        return Integer.parseInt(paramValue);
     }
 }
