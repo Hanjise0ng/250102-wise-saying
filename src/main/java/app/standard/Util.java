d083b99cf3bdd46e1760582f682dbd10648f67a5
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-06 14:14:51 +0900

    file test2 green

--- src/main/java/app/standard/Util.java
+++ src/main/java/app/standard/Util.java
@@ -1,10 +1,29 @@
 package app.standard;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Util {
 
     public static class File {
         public static void test() {
             System.out.println("파일 유틸 테스트");
+        }
+
+        public static void createFile(String pathValue) {
+            Path filepath = Paths.get(pathValue);
+            
+//            if (Files.exists(filepath)) {
+//                return;
+//            }
+            try {
+                Files.createFile(filepath);
+            } catch (Exception e) {
+                System.out.println("파일 생성 중 실패");
+                e.printStackTrace();
+            }
+
         }
     }
 
