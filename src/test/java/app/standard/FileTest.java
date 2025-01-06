465742f72ab40b09e366b1f6020bed8166f384c3
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-06 13:59:10 +0900

    fileUtil test setting

---
+++ src/test/java/app/standard/FileTest.java
@@ -0,0 +1,13 @@
+package app.standard;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FileTest {
+    @Test
+    @DisplayName("최초의 file test")
+    void t1(){
+        Util.File.test();
+    }
+
+}
