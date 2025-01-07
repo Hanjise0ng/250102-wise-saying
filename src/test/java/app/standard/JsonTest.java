9aa749f4ca856d08c25149c85f7d3e1942134af2
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-08 00:08:24 +0900

    json test1 red

---
+++ src/test/java/app/standard/JsonTest.java
@@ -0,0 +1,26 @@
+package app.standard;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonTest {
+
+    @Test
+    @DisplayName("Map을 Json으로 변환1 - 속성이 1개")
+    void t1() {
+        Map<String, Object> map = Map.of("name", "홍길동");
+
+        String jsonStr = Util.Json.mapToJson(map);
+
+        assertThat(jsonStr)
+                .isEqualTo("""
+                        {
+                            "name" : "홍길동"
+                        }
+                        """.stripIndent().trim());
+    }
+}
