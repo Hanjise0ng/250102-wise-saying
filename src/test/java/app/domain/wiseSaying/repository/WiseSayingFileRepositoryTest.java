55450d9dc1fa0d7934bc2305dbaa4ecbf848dbcd
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-09 09:19:55 +0900

    repo test4 red

--- src/test/java/app/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingFileRepositoryTest {
 
@@ -72,4 +73,23 @@ public class WiseSayingFileRepositoryTest {
         assertThat(foundWiseSaying).isNotNull();
         assertThat(foundWiseSaying).isEqualTo(wiseSaying);
     }
+
+    @Test
+    @DisplayName("모든 명언 가져오기")
+    void t4() {
+        WiseSaying wiseSaying1 = new WiseSaying(1, "aaa", "bbb");
+        WiseSaying wiseSaying2 = new WiseSaying(2, "ccc", "ddd");
+        WiseSaying wiseSaying3 = new WiseSaying(3, "eee", "fff");
+
+        wiseSayingRepository.save(wiseSaying1);
+        wiseSayingRepository.save(wiseSaying2);
+        wiseSayingRepository.save(wiseSaying3);
+
+        List<WiseSaying> wiseSayings = wiseSayingRepository.findAll();
+
+        assertThat(wiseSayings).hasSize(3);
+        assertThat(wiseSayings).contains(wiseSaying1, wiseSaying2, wiseSaying3);
+
+    }
+
 }
