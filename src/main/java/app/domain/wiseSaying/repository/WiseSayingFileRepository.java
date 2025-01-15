e15b37c498fe6d54fb48a616af4ad74ba0e95b9b
Hanjise0ng <tjdwl3269@gmail.com>
2025-01-15 22:05:47 +0900

    controller test13 green

--- src/main/java/app/domain/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/app/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public class WiseSayingFileRepository implements WiseSayingRepository {
 
-    private static final String DB_PATH = AppConfig.getDBPath();
+    private static final String DB_PATH = AppConfig.getDBPath() + "/wiseSaying";
     private static final String ID_FILE_PATH = DB_PATH + "/lastid.txt";
 
     public WiseSayingFileRepository() {
